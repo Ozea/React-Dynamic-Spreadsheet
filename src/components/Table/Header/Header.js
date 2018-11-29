424171a73d8ac97869cce1462e97c9bd97cbd2ad
Alexander <alexander@MacBook-Pro-Alexander.local>
2018-11-29 20:55:59 +0200

    Table header row editable content

--- src/components/Table/Header/Header.js
+++ src/components/Table/Header/Header.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
 
+import '../../../containers/App.css';
+
 class Header extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      edit: false,
+    }
+  }
+
+  saveChangedName = (evt) => {
+    this.setState({
+      edit: false
+    });
+    let newValue = evt.target.value;
+    this.props.handleChange(this.props.id, newValue);
+  }
+
+  replaceElement = () => {
+    const { editable } = this.props;
+    if(editable){
+      this.setState({
+        edit: true
+      });
+    }
+  }
+
   render(){
     return(
-      <th contentEditable={this.props.editable}>
-        {this.props.title}
+      <th onClick={this.replaceElement}>
+        {!this.state.edit && (
+          <span>{this.props.name}</span>          
+        )}
+        {this.state.edit && (
+          <input
+            defaultValue={this.props.name}
+            className="input"
+            onBlur={this.saveChangedName} />
+        )}
       </th>
     );
   }
